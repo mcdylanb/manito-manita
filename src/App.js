f5518cb9bde435617e5fc2cf98e9f566880cfbcc
dylan <dylansatgalab@gmail.com>
2021-12-21 00:48:39 +0000

    adds user input area

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ function App() {
       "Dylan","Kaitlyn", "Deion",
     ];
     const [parti, setParti] = useState(initialParticipants);
+  const [userInput, setUserInput] = useState('');
+
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value)
+  }
   return (
       <div>
+        <input value={userInput} type="text" onChange={handleChange} placeholder="Enter entry"/>
+        <button>Add</button>
         <ol>
           {parti.map((parti) => {
             return<li key={parti}>{parti}</li>;
