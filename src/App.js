7d31f6c2fbddf9a2f48919a27b56a9ea73a6d95a
dylan <dylansatgalab@gmail.com>
2021-12-21 01:15:35 +0000

    reads centralize data from a json file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, {useState} from 'react';
+
+import data from "./data.json";
+
 function App() {
-    const initialParticipants=[
-      "Dylan","Kaitlyn", "Deion",
-    ];
-    const [parti, setParti] = useState(initialParticipants);
+  const [parti, setParti] = useState(data);
   const [userInput, setUserInput] = useState('');
 
   const handleChange = (e) => {
     setUserInput(e.currentTarget.value)
   }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addParti(userInput)
+    setUserInput("")
+  }
+
+  const addParti = (userInput) => {
+    let copy = [...parti];
+    copy = [...copy, {id: parti.length + 1, name: userInput}];
+    setParti(copy)
+  }
   return (
       <div>
-        <input value={userInput} type="text" onChange={handleChange} placeholder="Enter entry"/>
-        <button>Add</button>
+        <form onSubmit={handleSubmit}>
+          <input value={userInput} type="text" onChange={handleChange} placeholder="Enter entry"/>
+          <button>Add</button>
+        </form>
         <ol>
           {parti.map((parti) => {
-            return<li key={parti}>{parti}</li>;
+            return<li key={parti.id}>{parti.name}</li>;
           })}
 
         </ol>
